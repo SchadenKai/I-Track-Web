126ff3bd2b0ad77587c8a58829a3f8845bf7e49e
Darren Malibiran <darrenmalibiran@outlook.com>
2022-09-29 19:16:01 +0800

    Basic Login Form

---
+++ login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required Meta tags -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- Page Title -->
+    <title>Login | I-Track</title>
+
+</head>
+<body>
+
+<h1>I-Track</h1>
+
+<form>
+    <div>
+        <label for="email">Email Address</label>
+        <input type="email" id="email" name="email" placeholder="Enter email address" required>
+    </div>
+    <div>
+        <label for="password">Password</label>
+        <input type="password" id="password" name="password" placeholder="Enter password" required>
+    </div>
+    <button type="submit" id="login" name="login">
+        LOGIN
+    </button>
+</form>
+    
+</body>
+</html>
