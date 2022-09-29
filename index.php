a9fe21913589e741c7553d889b8a55999bdbdbab
Darren Malibiran <darrenmalibiran@outlook.com>
2022-09-29 22:27:00 +0800

    Added login session

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
+<?php 
+
+session_start();
+
+	if(!isset($_SESSION['userlogin'])){
+		header("Location: login.php");
+	}
+
+	if(isset($_GET['logout'])){
+		session_destroy();
+		unset($_SESSION);
+		header("Location: login.php");
+	}
+
+?>
+
 <p>This is the landing page after login</p>
+
+<a href="index.php?logout=true">Logout</a>
