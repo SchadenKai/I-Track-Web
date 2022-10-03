df7343407ab6e5b7d59ba87a048dfb44a18fdf25
Darren Malibiran <darrenmalibiran@outlook.com>
2022-10-03 12:21:47 +0800

    Added sample datastudio dashboard

--- dashboard.php
+++ dashboard.php
@@ -22,6 +22,6 @@ session_start();
 <script>
 document.title = "I-Track | Dashboard"
 </script>
+<h1 style="text-align: center;">This is a sample dashboard</h1>
+<iframe style="position: absolute; width:100%; height: 100%; border: 0; margin:2px;" src="https://datastudio.google.com/embed/reporting/1Lkg3k30dv3POg80rm3sQk06BVi9hqVES/page/1M"></iframe>
 
-<!-- Content of the Page -->
-<h1 style="text-align: center;">This is the Dashboard Page</h1>
