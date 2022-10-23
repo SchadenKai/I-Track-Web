784859138f80b01671047a947244b80037fcb90c
Darren Malibiran <darrenmalibiran@outlook.com>
2022-10-23 10:58:35 +0800

    Fixed Structure

--- app/about.php
+++ app/about.php
@@ -14,14 +14,14 @@ session_start();
 	}
 
 	// Include the Navigation Bar header. 
-	include "nav.html"
+	include "utils/nav.html"
 
 ?>
 
 <!-- Change the Document Title-->
 <script>
-document.title = "I-Track | Dashboard"
+document.title = "I-Track | About"
 </script>
-<h1 style="text-align: center;">This is a sample dashboard</h1>
-<iframe style="position: absolute; width:100%; height: 100%; border: 0; margin:2px;" src="https://datastudio.google.com/embed/reporting/1Lkg3k30dv3POg80rm3sQk06BVi9hqVES/page/1M"></iframe>
 
+<!-- Content of the Page -->
+<h1 style="text-align: center;">This is the About Page</h1>
