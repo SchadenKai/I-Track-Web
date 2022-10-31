431fe0c1386e588e011199e3876904dd8258c5ef
Darren Malibiran <darrenmalibiran@outlook.com>
2022-10-31 10:49:31 +0800

    Compare encrypted md5 password to database entries

--- app/modules/validate_user.php
+++ app/modules/validate_user.php
@@ -5,11 +5,11 @@ require_once('../configs/psql_conn.php');
 
 // Login Credentials
 $usr_email = $_POST['email'];
-$usr_pw = $_POST['password'];  
+$usr_pw = md5($_POST['password']);  
 
 
 // Check the existence of user login credentials in the database
-$sql = "SELECT * FROM users WHERE email = $1 AND password = $2 LIMIT 1;";
+$sql = "SELECT * FROM admin WHERE email = $1 AND password = $2 LIMIT 1;";
 $result = pg_prepare($db, "check_query", $sql);
 $result = pg_execute($db, "check_query", array($usr_email, $usr_pw));
 
