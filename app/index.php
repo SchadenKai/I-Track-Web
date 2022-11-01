6164467e8540ff01fa1537072a909693ee7c6299
Kai Tecson <kairustecsonpersonal@gmail.com>
2022-11-01 09:02:57 +0800

    added documentation

--- app/index.php
+++ app/index.php
@@ -11,6 +11,7 @@ document.title = "I-Track | Home"
 <div class="d-flex w-100 h-100 justify-content-center pt-5">
 	<!-- Content Container -->
     <div class="shadow border w-100 h-75 mx-5"> 
+
 		<!-- I-track Nav -->
         <nav class="nav nav-pills nav-fill custom-navbar">
             <li class="nav-item">
@@ -32,15 +33,18 @@ document.title = "I-Track | Home"
                 </a>
             </li>
 		</nav>
-		<section class="container-fluid p-3 ps-5"> <!-- Main Content Wrapper -->
-            <!-- Top Section -->
+        
+        <!-- Top Section Functionalities-->
+		<section class="container-fluid p-3 ps-5"> 
 			<h3 class="d-block ">CLASS 1 AY 2022-2023</h3>
 			<p class="text-secondary fs-6">
 				<i class="bi bi-info-circle me-2"></i>
 				Click the “Filter Options” button to change the class
 			</p>
 			<div class="d-flex w-100 justify-content-between mt-3">
-				<button class="btn btn-outline-dark">Filter Options</button>
+                <!-- Filter Button; create pop up menu-->
+				<button class="btn btn-outline-dark">Filter Options</button> 
+                <!-- Search functionality; search through the DB -->
 				<div class="input-group" style="width: 300px;">
 					<input type="text" class="form-control" placeholder="Search Students" aria-label="student-name" aria-describedby="input-group-right">
 					<span class="input-group-text" id="input-group-right-example">
@@ -49,8 +53,10 @@ document.title = "I-Track | Home"
 				</div>
 			</div>
         </section>
-        <section class="d-flex flex-column container-fluid align-items-center">
-            <div class="d-flex flex-column border shadow w-75 h-auto my-5 text-center"> <!-- Visible Container -->
+
+        <section class="d-flex flex-column container-fluid align-items-center"> <!-- Fill width; center content -->
+            <!-- Class List Container -->
+            <div class="d-flex flex-column border shadow w-75 h-auto my-5 text-center"> 
                 <div class="row border-0 bg-white fw-bold text-white">
                     <div class="col border py-3" style="background-color: #133379;">No.</div>
                     <div class="col border py-3" style="background-color: #133379;">First Name</div>
@@ -58,7 +64,8 @@ document.title = "I-Track | Home"
                     <div class="col border py-3" style="background-color: #133379;">Last Name</div>
                     <div class="col border py-3" style="background-color: #133379;">Status</div>
                 </div>
-                <a href="/pages/dashboard.php" class="alink-color">
+                <!-- Row Header -->
+                <a href="/pages/dashboard.php">
                     <div class="row border-0 bg-white">
                         <div class="col border">No.</div>
                         <div class="col border">First Name</div>
@@ -67,7 +74,7 @@ document.title = "I-Track | Home"
                         <div class="col border">Status</div>
                     </div>
                 </a>
-                
+            
                 <div class="row border-0 bg-white">
                     <div class="col border">No.</div>
                     <div class="col border">First Name</div>
@@ -88,8 +95,8 @@ document.title = "I-Track | Home"
                     <div class="col border">Middle Initial</div>
                     <div class="col border">Last Name</div>
                     <div class="col border">Status</div>
-                </div>
-                </div>
+                <!-- JS Add element this format to add students -->
+                <!-- Retrieve data from Database -->
             </div>
         </section>
     </div>
