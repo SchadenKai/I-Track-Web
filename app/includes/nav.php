dd2bea8fb037835468505d7f4e8f9dd5d6cf021a
Kai Tecson <kairustecsonpersonal@gmail.com>
2022-11-07 23:00:55 +0800

    added jquery cdn

--- app/includes/nav.php
+++ app/includes/nav.php
@@ -13,6 +13,8 @@
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>    
     <!-- Bootstrap icon CDN-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <!-- JQuery CDN -->
+    <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
     <!-- External CSS -->
     <link rel="stylesheet" type="text/css" href="../css/styles.css">
 </head>
