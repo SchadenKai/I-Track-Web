bac54f804f5765e2423abf294156d72185389f44
Darren Malibiran <darrenmalibiran@gmail.com>
2023-03-31 14:10:31 +0800

    Separated CS curriculum to JSON flat file

--- setup/generate_data.py
+++ setup/generate_data.py
@@ -3,12 +3,13 @@ import names
 import datetime
 import csv
 import logging
+import json
 from password_generator import PasswordGenerator
 from scipy.stats import skewnorm
 
 class DataGenerator:
     def __init__(self):
-         # in 1 semester(5 months: 2 exam, weekly quiz and activity. Set the attendance for class A as MTW, B as WThF, C as FSS
+        # in 1 semester(5 months: 2 exam, weekly quiz and activity. Set the attendance for class A as MTW, B as WThF, C as FSS
         # Semester 2 starts in week 4 (end of Jan). Semester 1 in week 31 (start of Aug)
         self.semester_starting_week = {
             1: 31,
@@ -22,89 +23,10 @@ class DataGenerator:
             "C": [5,6,7]
         }
 
-         # Computer Science curriculum
-        self.curriculum = {
-            "First Year": {
-                1: [
-                    {"subject_id": "IT 111", "subject_name": "Introduction to Computing", "units": 3},
-                    {"subject_id": "CS 111", "subject_name": "Computer Programming", "units": 3},
-                    {"subject_id": "Fili 101", "subject_name": "Kontekstwalisadong Komunikasyon sa Filipino", "units": 3},
-                    {"subject_id": "GEd 101", "subject_name": "Understanding the Self", "units": 3},
-                    {"subject_id": "GEd 102", "subject_name": "Mathematics in the Modern World", "units": 3},
-                    {"subject_id": "Math 111", "subject_name": "Linear Algebra", "units": 3},
-                    {"subject_id": "PE 101", "subject_name": "Physical Fitness, Gymnastics and Aerobics", "units": 2}
-                ],
-                2: [
-                    {"subject_id": "CS 121", "subject_name": "Advanced Computer Programming", "units": 3},
-                    {"subject_id": "Fili 102", "subject_name": "Filipino sa Iba't Ibang Disiplina", "units": 3},
-                    {"subject_id": "GEd 105", "subject_name": "Readings in Philippine History", "units": 3},
-                    {"subject_id": "GEd 108", "subject_name": "Art Appreciation", "units": 3},
-                    {"subject_id": "Math 401", "subject_name": "Differential Calculus", "units": 3},
-                    {"subject_id": "Math 407", "subject_name": "Number Theory", "units": 3},
-                    {"subject_id": "PE 102", "subject_name": "Rhythmic Activities", "units": 2}
-                ]
-            },
-            "Second Year": {
-                1: [
-                    {"subject_id": "CS 211", "subject_name": "Object-Oriented Programming", "units": 3},
-                    {"subject_id": "CS 212", "subject_name": "Computer Organization w/ Assembly Language", "units": 3},
-                    {"subject_id": "IT 211", "subject_name": "Database Management Systems", "units": 3},
-                    {"subject_id": "IT 212", "subject_name": "Computer Networking 1", "units": 3},
-                    {"subject_id": "Phy 101", "subject_name": "Calculus-Based Physics", "units": 3},
-                    {"subject_id": "CpE 405", "subject_name": "Discrete Mathematics", "units": 3},
-                    {"subject_id": "GEd 109", "subject_name": "Science, Technology and Society", "units": 3},
-                    {"subject_id": "PE 103", "subject_name": "Individual and Dual Sports", "units": 2}
-                ],
-                2: [
-                    {"subject_id": "CS 221", "subject_name": "Design and Analysis of Algorithms", "units": 3},
-                    {"subject_id": "CS 222", "subject_name": "Advanced Object-Oriented Programming", "units": 3},
-                    {"subject_id": "IT 221", "subject_name": "Information Management", "units": 3},
-                    {"subject_id": "GEd 106", "subject_name": "Purposive Communication", "units": 3},
-                    {"subject_id": "GEd 107", "subject_name": "Ethics", "units": 3},
-                    {"subject_id": "ES 101", "subject_name": "Environmental Sciences", "units": 3},
-                    {"subject_id": "ENGG 414", "subject_name": "Numerical Methods", "units": 3}, 
-                    {"subject_id": "PE 104", "subject_name": "Team Sports", "units": 2}                
-                ]
-            },
-            "Third Year": {
-                1: [
-                    {"subject_id": "CS 311", "subject_name": "Automata Theory and Formal Languages", "units": 3},
-                    {"subject_id": "CS 312", "subject_name": "Mobile Computing", "units": 3},
-                    {"subject_id": "IT 321", "subject_name": "Human Computer Interaction", "units": 3},
-                    {"subject_id": "IT 314", "subject_name": "Web Systems and Technologies", "units": 3},
-                    {"subject_id": "IT 331", "subject_name": "Application Development and Emerging Technologies", "units": 3},
-                    {"subject_id": "Math 408", "subject_name": "Data Analysis", "units": 3},
-                    {"subject_id": "GEd 104", "subject_name": "The Contemporary World", "units": 3}
-                ],
-                2: [
-                    {"subject_id": "CS 321", "subject_name": "Programming Languages", "units": 3},
-                    {"subject_id": "CS 322", "subject_name": "Software Engineering", "units": 3},
-                    {"subject_id": "CS 324", "subject_name": "Modeling and Simulation", "units": 3},
-                    {"subject_id": "Math 409", "subject_name": "Symbolic Logic", "units": 3},
-                    {"subject_id": "GEd 103", "subject_name": "Life and Works of Rizal", "units": 3},
-                    {"subject_id": "NTT 404", "subject_name": "Cloud Computing", "units": 3}                
-                ]
-            },
-            "Fourth Year": {
-                1: [
-                    {"subject_id": "CS 411", "subject_name": "CS Thesis 1", "units": 3},
-                    {"subject_id": "CS 412", "subject_name": "Fundamentals of Data Science", "units": 3},
-                    {"subject_id": "CS 413", "subject_name": "Advanced Software Engineering", "units": 3},
-                    {"subject_id": "CS 414", "subject_name": "Artificial Intelligence", "units": 3},
-                    {"subject_id": "CS 415", "subject_name": "Principles of Operating Systems", "units": 3},
-                    {"subject_id": "CS 424", "subject_name": "Parallel and Distributed Computing", "units": 3}
-                ],
-                2: [
-                    {"subject_id": "CS 421", "subject_name": "CS Thesis 2", "units": 3},
-                    {"subject_id": "CS 422", "subject_name": "Machine Learning", "units": 3},
-                    {"subject_id": "CS 423", "subject_name": "Social Issues and Professional Practice", "units": 3},
-                    {"subject_id": "IT 323", "subject_name": "Information Assurance and Security", "units": 3},
-                    {"subject_id": "ENGG 405", "subject_name": "Technopreneurship", "units": 3},
-                    {"subject_id": "CS 425", "subject_name": "Systems Fundamentals", "units": 3}
-                ]
-            }
-        }
-
+        # Computer Science curriculum
+        with open("setup/data/curriculum.json") as f:
+            self.curriculum = json.load(f)
+        
     @staticmethod        
     def write_csv(filepath, all_data):
         """
@@ -252,7 +174,7 @@ class DataGenerator:
         >>> print(__get_subjects(1))        
         CS 121, Fili 102, GEd 105, ...
         """
-        return ", ".join([subject["subject_id"] for subject in self.curriculum[year_lvl][random.choice([1,2])]])        
+        return ", ".join([subject["subject_id"] for subject in self.curriculum[year_lvl][str(random.choice([1,2]))]])        
         
 
     def __get_learning_style(self):
