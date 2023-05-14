07e0826fdcb101d6630dadd712c3659ccddd56a5
Darren Malibiran <darrenmalibiran@gmail.com>
2023-05-14 11:55:50 +0800

    Insert bulletin announcement to database

--- app/pages/bulletin.php
+++ app/pages/bulletin.php
@@ -2,6 +2,7 @@
 	require "../Model/modules/validation.php";
     include "../View/includes/nav.php";
     include "../View/includes/filterOption.html";
+    require "../Model/configs/psql_conn.php";
 ?>
 
 <!-- Change the Document Title-->
@@ -65,7 +66,7 @@ document.title = "I-Track | Bulletin"
             <!-- Class List Container -->
             <form class="d-flex flex-column border rounded h-auto m-3 py-3 px-5 text-center " style="width: 85%; background-color: rgb(185, 185, 185)" method="post" action=""> 
                 <div class="form-floating">
-                    <textarea class="form-control shadow" placeholder="Leave a comment here" id="floatingTextarea" style="height: 250px"></textarea>
+                    <textarea class="form-control shadow" name="announcement" placeholder="Leave a comment here" id="floatingTextarea" style="height: 250px"></textarea>
                     <label for="floatingTextarea">Announce something...</label>
                 </div>
                 <button type="submit" class="btn btn-success mt-3 align-self-end rounded-pill" style="min-width: 150px;">Upload</button>
@@ -76,3 +77,24 @@ document.title = "I-Track | Bulletin"
 </div>
 <script src="../View/assets/js/eventHandles.js" type="text/javascript" async></script>
 <script type="module" src="../Controller/otherMainController.js" async></script>
+<?php
+// Check if the form has been submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Get the announcement from the form data
+    $announcement = $_POST['announcement'];
+
+    // Fetch classes of the currently logged in admin
+    $sql = "select class_id from class where admin_id = $1;";
+    $result = pg_prepare($db, "fetch_class_students_query", $sql);
+    $result = pg_execute($db, "fetch_class_students_query", array($_SESSION['userlogin']['id']));
+    $classes_of_admin = pg_fetch_all($result);    
+    for ($i = 0; $i < count($classes_of_admin); $i++){
+        // Insert the announcement into the database
+        
+        $result = pg_query_params($db, "INSERT INTO bulletin(content, attachment_url, time_created, class_id) VALUES($1, $2, NOW(), $3);", array($announcement, '', $classes_of_admin[$i]['class_id']));        
+    }
+
+    // Clear the textarea
+    echo "<script>document.getElementById('floatingTextarea').value = '';</script>";
+}
+?>
