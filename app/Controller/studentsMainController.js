1298b9ebebbf83a70aa3edad55cc4c4ce0257492
Darren Malibiran <103012607+darren-sm@users.noreply.github.com>
2023-05-14 01:31:03 +0000

    Fixed search function

--- app/Controller/studentsMainController.js
+++ app/Controller/studentsMainController.js
@@ -10,19 +10,32 @@ $.get("../Model/modules/app_data.json",
     }
 )
 
-$('#search-box').keydown(()=> {
-    
-    let studentName = $("#search-box").val()
-    
-    $.post("../../Model/modules/filter_students.php",
-        {suggestion: studentName},
-        function(data,status) {
-            const obj = JSON.parse(data)
-            insertTemplate_studentList(obj)
-            console.log(status);
-        }
-    )
-  })
+let debounceTimeout;
+
+$('#search-box').on('input', ()=> {
+    const studentName = $("#search-box").val();
+
+    // Clear previous debounce timeout
+    clearTimeout(debounceTimeout);
+
+    // Set new debounce timeout
+    debounceTimeout = setTimeout(() => {
+        $.post("../../Model/modules/filter_students.php",
+            {suggestion: studentName},
+            function(data,status) {
+                const obj = JSON.parse(data);
+                if (obj.length === 0) {
+                    // Display "no value" if there are no results
+                    $('#student-list').html('<p>No value</p>');
+                } else {
+                    // Display results if there are any
+                    insertTemplate_studentList(obj);
+                }
+                console.log(status);
+            }
+        )
+    }, 300); // Change delay time as needed
+});
 
 
 $(document).ready(()=>{
